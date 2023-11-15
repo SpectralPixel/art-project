990e452b6a46dcff959ff89c6ed9cff47c9842d7
makki the destroyer <wulin007@seznam.cz>
2023-11-15 13:44:27 +0100

    bluhh bluh

--- src/map.rs
+++ src/map.rs
@@ -1,3 +1,13 @@
 pub fn test_function() {
     println!("map works!");
 }
+pub struct Color{
+    r:u8,
+    g:u8,
+    b:u8,
+}
+pub struct Map{
+    width: u32,
+    height: u32,
+    red_layer: 
+}
