f86b51b87ac97ba282d0b2f01fbed1065c697d6e
SpectralPixel <encrypteddvjjrxv@gmail.com>
2023-11-22 19:38:34 +0100

    moved map_size out of main()

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ use bevy::time::prelude::Fixed;
 use bevy_pixel_buffer::prelude::*;
 
 const UPDATE_RATE: f32 = 0.25;
+const MAP_SIZE: PixelBufferSize = PixelBufferSize {
+    size: UVec2::new(32, 32),       // amount of pixels
+    pixel_size: UVec2::new(16, 16), // size of each pixel in the screen
+};
 
 fn main() {
     println!("i like cats");
@@ -13,23 +17,18 @@ fn main() {
     map::green_layer::test_function();
     map::blue_layer::test_function();
     map::test_function();
-    
-    let map_size = PixelBufferSize {
-        size: UVec2::new(32, 32),       // amount of pixels
-        pixel_size: UVec2::new(16, 16), // size of each pixel in the screen
-    }; // Setup system
 
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, pixel_buffer_setup(map_size))
+        .add_systems(Startup, pixel_buffer_setup(MAP_SIZE))
         // FixedUpdate runs a set amount of times every seconds, and is independent from screen updates
-        .add_systems(FixedUpdate, pixel_update)
+        .add_systems(FixedUpdate, update_screen)
         // set FixedUpdate rate
-        .insert_resource(Time::<Fixed>::from_seconds(0.25))
+        .insert_resource(Time::<Fixed>::from_seconds(0.1))
         .run();
 }
 
-fn pixel_update(mut pb: QueryPixelBuffer) {
+fn update_screen(mut pb: QueryPixelBuffer) {
     // Set each pixel to a random color
     pb.frame().per_pixel(|_, _| Pixel::random());
 }
