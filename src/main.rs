d94752b485007c470c45aebac7b9c4986c960391
SpectralPixel <encrypteddvjjrxv@gmail.com>
2023-11-22 12:22:55 +0100

    added pixel buffer crate and boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod map;
 
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use bevy::prelude::*;
+use bevy_pixel_buffer::prelude::*;
 
 fn main() {
     println!("i like cats");
@@ -10,58 +11,19 @@ fn main() {
     map::blue_layer::test_function();
     map::test_function();
 
+    let map_size = PixelBufferSize {
+        size: UVec2::new(32, 32),       // amount of pixels
+        pixel_size: UVec2::new(16, 16), // size of each pixel in the screen
+    }; // Setup system
+
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup) // https://bevyengine.org/examples/2D%20Rendering/2d-shapes/
-        .add_systems(Update, hello_world)
+        .add_systems(Startup, pixel_buffer_setup(map_size))
+        .add_systems(Update, pixel_update)
         .run();
 }
 
-fn hello_world() {
-    println!("THIS CODE RUNS EVERY FRAME!");
-}
-
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
-    commands.spawn(Camera2dBundle::default());
-
-    // Circle
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes.add(shape::Circle::new(50.).into()).into(),
-        material: materials.add(ColorMaterial::from(Color::PURPLE)),
-        transform: Transform::from_translation(Vec3::new(-150., 0., 0.)),
-        ..default()
-    });
-
-    // Rectangle
-    commands.spawn(SpriteBundle {
-        sprite: Sprite {
-            color: Color::rgb(0.25, 0.25, 0.75),
-            custom_size: Some(Vec2::new(50.0, 100.0)),
-            ..default()
-        },
-        transform: Transform::from_translation(Vec3::new(-50., 0., 0.)),
-        ..default()
-    });
-
-    // Quad
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes
-            .add(shape::Quad::new(Vec2::new(50., 100.)).into())
-            .into(),
-        material: materials.add(ColorMaterial::from(Color::rgb(1., 1., 0.))),
-        transform: Transform::from_translation(Vec3::new(50., 0., 0.)),
-        ..default()
-    });
-
-    // Hexagon
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes.add(shape::RegularPolygon::new(50., 6).into()).into(),
-        material: materials.add(ColorMaterial::from(Color::TURQUOISE)),
-        transform: Transform::from_translation(Vec3::new(150., 0., 0.)),
-        ..default()
-    });
+fn pixel_update(mut pb: QueryPixelBuffer) {
+    // Set each pixel to a random color
+    pb.frame().per_pixel(|_, _| Pixel::random());
 }
